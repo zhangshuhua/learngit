c1107dd52ea360f677e50bfdbc83f3687fbf1614
张树华 <zhangshuhua411@hotmail.com>
2017-06-30 17:27:53 +0800

    Test.class

---
+++ Test.java
@@ -0,0 +1,13 @@
+
+//Git is a vsrsion control system
+//Git is fress software
+
+public class Test{
+   int a = 1;
+   int b = 2;
+   public static void main(String[] args){
+      
+       Test t = new Test();
+       System.out.print(t.a+t.b);
+   }
+}
